b2abd5de98c4135a2de7999c418706e3281a1203
hex25 <natasha.minin@gmail.com>
2019-09-09 10:27:11 +0200

    session countdown added, pause button working

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let timerValue = 1500;
+
+document.getElementById('time-left').innerHTML = timeFormat(timerValue);
+
 document.getElementById('reset').onclick = function() {
+    stopTimer();
     document.getElementById('break-length').innerHTML = 5;
     document.getElementById('session-length').innerHTML = 25;
-    // and a timer should stop
+    timerValue = 1500;
+    document.getElementById('time-left').innerHTML = timeFormat(timerValue);
+
   }
   document.getElementById('break-decrement').onclick = function() {
     
@@ -25,3 +32,47 @@ document.getElementById('reset').onclick = function() {
     document.getElementById('session-length').innerHTML = parseInt(document.getElementById('session-length').innerHTML) + 1;
   }
   }
+    
+  function timeFormat(time) {
+    let minutes =  Math.floor(time/60);
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }   
+    let seconds = time%60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`
+ }   
+
+
+let startTimer;
+
+function stopTimer() {
+    clearInterval(startTimer);
+}
+
+function countDown() {
+    if (timerValue > 0) { 
+  timerValue -= 1;
+  document.getElementById('time-left').innerHTML = timeFormat(timerValue);
+    }
+}
+
+let isClicked = false;
+
+document.getElementById('start_stop').onclick = function() {
+    if (isClicked === false) {
+        startTimer = setInterval(countDown, 1000);
+        document.getElementById('play-icon').src = './pause.png';
+        document.getElementById('play-icon').style.width = '90%';
+        isClicked = true;
+    } else {
+        stopTimer();
+        document.getElementById('play-icon').src = './play.png';
+        document.getElementById('play-icon').style.width = '100%';
+        isClicked = false;
+    }
+    
+  }
+
